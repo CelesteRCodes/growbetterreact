b1a353f57629a8d7d9d4a540593d2ab85ef347d2
Celeste R Codes <celestercodes@gmail.com>
2021-07-25 03:38:19 -0400

    change file locations. update imports.

--- src/components/Plant/UserPlants.js
+++ src/components/Plant/UserPlants.js
@@ -1,10 +1,10 @@
 import React from 'react';
 import DetailsCard from './DetailsCard';
-import GrowLog from './GrowLog';
+import GrowLog from '../GrowLog';
 import NewPlant from './NewPlant';
 import PlantCard from './PlantCard';
 import PlantLog from './PlantLog';
-import NewEntry from './NewEntry';
+import NewEntry from '../NewEntry';
 
 function ShowUserPlants(props) {
     return (
